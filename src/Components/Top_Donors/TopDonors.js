b2d4fac340567bbe2dfbe8803e44fdc586663e42
Sentinail <wilsongponseca@gmail.com>
2023-06-30 23:43:32 +0800

    add top donors page -Sentinail

--- src/Components/Top_Donors/TopDonors.js
+++ src/Components/Top_Donors/TopDonors.js
@@ -1,21 +1,54 @@
-import React from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import { TopDonorsContainer, TopDonorCellStyle } from './TopDonorsStyles'
+import { stylesContext } from '../../ContextProviders/StylesProvider'
+import axios from 'axios'
 
 export const TopDonorCell = (props) => {
+  const {name, totalDonation, rankNum, backgroundColor} = props
 
   return (
-    <TopDonorCellStyle>
-      <p>Jennie Nichols</p>
-      <p>$1000</p>
-      <p>#1</p>
+    <TopDonorCellStyle backgroundColor={backgroundColor}>
+      <p className='name'>{name}</p>
+      <p className='total-donation'>${totalDonation}</p>
+      <p className='rank-num'>#{rankNum}</p>
     </TopDonorCellStyle>
   )
 }
 
 function TopDonors() {
+  const styles = useContext(stylesContext)
+  const [data, setData] = useState()
+
+  const getUsersSorted = async () => {
+    const data = await axios.get("http://localhost:5000/users")
+    const users = await data.data
+    users.sort((a, b) => b.totalDonation - a.totalDonation);
+    return users
+    
+  }
+
+  useEffect(() => {
+    getUsersSorted().then(res => {setData(res)})
+  }, [])
+
+  console.log(data)
+
+ 
+
+
   return (
     <TopDonorsContainer>
-
+      <div className='board'>
+        <h1 className='title'>TOP DONORS</h1>
+        <div className='cells'>
+          {data ? data.map((user, index) => {return <TopDonorCell 
+          key={index} 
+          name={user.fullName} 
+          totalDonation={user.totalDonation} 
+          rankNum={index + 1} 
+          backgroundColor={(index + 1) % 2 === 0 ? styles.primaryColor : styles.secondaryColor}></TopDonorCell>}) : "False"}
+        </div>
+      </div>
     </TopDonorsContainer>
   )
 }
--- src/Components/Top_Donors/TopDonorsStyles.js
+++ src/Components/Top_Donors/TopDonorsStyles.js
@@ -1,8 +1,45 @@
 import styled from "styled-components";
 
 export const TopDonorsContainer = styled.div`
+    width: 100%;
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    padding: 30px;
 
-`
+    & .board {
+        margin-top: 100px;
+        margin-bottom: 100px;
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 60px;
+        animation: fadeIn 0.5s ease-in-out;
+
+        @keyframes fadeIn {
+            from { opacity: 0; transform: translateY(-50px);}
+            to { opacity: 1; transform: translateY(0px);}
+        }
+
+        & .title {
+            font-size: 6rem;
+        }
+
+        & .cells {
+            width: 100%;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            gap: 30px;
+
+            & p {
+                font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+            }
+        }
+    }
+`   
 
 export const TopDonorCellStyle = styled.div`
     width: 100%;
@@ -12,11 +49,35 @@ export const TopDonorCellStyle = styled.div`
     border-radius: 20px;
 
     display: flex;
-    justify-content: space-between;
+    justify-content: center;
     align-items: center;
     padding: 20px;
 
-    background-color: #2F2E2E;
+    background-color: ${props => {return `rgb(${props.backgroundColor[0]}, ${props.backgroundColor[1]}, ${props.backgroundColor[2]}, ${props.backgroundColor[3]})`}};
     font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
     color: #FFFFFF;
+    position: relative;
+
+    & .name {
+        position: absolute;
+        top: 50%;
+        transform: translateY(-50%);
+        left: 30px;
+    }   
+
+    & .total-donation {
+        position: absolute;
+        top: 50%;
+        right: 50%;
+        transform: translateY(-50%) translateX(50%);
+
+    }
+
+    & .rank-num {
+        position: absolute;
+        top: 50%;
+        transform: translateY(-50%);
+        right: 30px;
+
+    }
 `
